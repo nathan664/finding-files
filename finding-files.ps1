4b72052e2890c55a2520c9899dd124b74228b5f8
nathan664 <75317247+nathan664@users.noreply.github.com>
2021-03-26 07:45:55 -0400

    Add files via upload

---
+++ finding-files.ps1
@@ -0,0 +1,13 @@
+$file = Read-Host -Prompt "what type of file would you like to choose (.jpg, .txt, .jpeg, .png .pdf)"
+
+if($file -eq '.jpg'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.jpg) }else { write-host('check your home screen for a text document labeled output') }.j
+
+if($file -eq '.txt'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.txt) }else { write-host('check your home screen for a text document labeled output') }
+
+if($file -eq '.jpeg'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.jpeg) }else { write-host('check your home screen for a text document labeled output') }
+
+if($file -eq '.png'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.png) }else { Write-host('check your home screen for a text document labeled output') }
+
+if($file -eq '.gif'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.gif) }else { write-host('check your home screen for a text document labeled output') }
+
+if($file -eq '.pdf'){ Add-Content $home\Desktop\output.txt (Get-ChildItem 'C:' -Recurse *.pdf) }else { write-host('check your home screen for a text document labeled output') }
